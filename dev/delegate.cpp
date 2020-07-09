0b456902ec2481e8014c28b0492da68fb85bd54a
Librianets <librianets@gmail.com>
2020-07-09 12:10:42 +0700

    dev 2.0

---
+++ dev/delegate.cpp
@@ -0,0 +1,9 @@
+#include <windows.h>		//win
+#include <ostream>		//
+#include <iostream>		//
+
+#include <list>
+#include <cstdio>
+//#include <typeinfo> // Для std::bad_cast
+//#include <iostream> // Для std::cerr и др.
+#include "delegate.hpp"
---
+++ dev/shell.sh
@@ -0,0 +1,1 @@
+i686-w64-mingw32-gcc memorymanager.cpp delegate.cpp main.cpp -o main.exe -lstdc++ -lcomctl32 -lkernel32 -lcomdlg32 -luser32 -lgdi32 -static -static-libgcc -static-libstdc++ -mwindows -municode 
